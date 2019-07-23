68aa96f4dcffa9a2f78074436222cf6874f249af
Your Name <itzalbert2@gmail.com>
2019-07-23 14:49:27 -0700

    Add a few helper functions

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -2,14 +2,11 @@ const puppeteer = require('puppeteer');
 const { username, password } = require('../config/config_test_file.js');
 
 /**
- * @description: Running this function with headless set to true triggers a captcha page and will break
- * the function. Always set headless to be false in order to not trigger captcha page.
  *
+ * @param {string} page
+ * @description: page refers to the browser page that is instantiated with a headless value of false
  */
-
-const logIn = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+const logIn = async page => {
   const loginPage = 'https://angel.co/login';
   const emailTextField = '#user_email';
   const passwordTextField = '#user_password';
@@ -27,4 +24,60 @@ const logIn = async () => {
   }
 };
 
-logIn();
+/**
+ * @param {class} page
+ * @param {string} selector
+ *
+ * @description: Page should be instantiated prior to being passed into this function. The selector
+ * should be the element you are trying to get the text value out of.
+ */
+
+const getTextValue = async (page, selector) => {
+  let innerText;
+
+  try {
+    await page.waitForSelector(selector);
+    const element = await page.$(selector);
+    innerText = page.evaluate(element => element.textContent, element);
+  } catch {
+    innerText = '';
+  }
+  return innerText;
+};
+
+//TODO: Pass in page, and eliminate the need to open up a new page
+const parseCompanyAndJobTitle = async url => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  const header = '.u-colorGray3';
+  const companyAndTitle = await getTextValue(page, header);
+  const splitHeader = companyAndTitle.split(' at ');
+  const jobTitle = splitHeader[0];
+  const company = splitHeader[1];
+
+  return [jobTitle, company];
+};
+
+const apply = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await logIn(page);
+
+  let result = await getTextValue(page, '.startup-link');
+};
+
+apply();
+
+/*
+TODO: 
+- Write the following functions: 
+  - Get company name
+  - Get recruiter name
+  - Get position
+  - Format recruiter name
+  
+*/
