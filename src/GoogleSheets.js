d93b6aee9b3d8ec196ae827b84487fbfb8ce7fae
Your Name <itzalbert2@gmail.com>
2019-07-25 21:59:07 -0700

    Clean up GoogleSheet class

--- src/GoogleSheets.js
+++ src/GoogleSheets.js
@@ -4,48 +4,38 @@ const { spreadsheetId } = require('../config/config_test_file');
 const { promisify } = require('util');
 
 /**
- *
  * @description: This function will automatically grab all of the rows within the first spreadsheet
  * that you have listed, and returns back an array of objects that contain all the columns/rows.
- *
- * Data shape is:
- * {
- * _xml:
- * id
- * _links:
- * all the headers
- * save: function
- * del: function
- * }
  */
-const getSpreadsheetAccess = async () => {
-  const doc = new GoogleSpreadsheet(spreadsheetId);
-  await promisify(doc.useServiceAccountAuth)(creds);
-  const sheetInfo = await promisify(doc.getInfo)();
-  const currentSheet = sheetInfo.worksheets[0];
 
-  const rows = await promisify(currentSheet.getRows)({
-    offset: 1,
-    limit: 120,
-    query: 'applied = no and hashyperlink = true'
-  });
+module.exports = {
+  getSpreadsheetAccess: async () => {
+    const doc = new GoogleSpreadsheet(spreadsheetId);
+    await promisify(doc.useServiceAccountAuth)(creds);
+    const sheetInfo = await promisify(doc.getInfo)();
+    const currentSheet = sheetInfo.worksheets[0];
 
-  /**
-   * @description: Return value rows is an array of spreadsheet items for that specific sheet.
-   */
-  return rows;
+    const rows = await promisify(currentSheet.getRows)({
+      offset: 1,
+      limit: 120,
+      query: 'applied = no and hashyperlink = true'
+    });
+
+    /**
+     * @description: Return value rows is an array of spreadsheet items for that specific sheet.
+     */
+
+    return rows;
+  },
+
+  writeToSpreadsheet: async () => {
+    const rows = await getSpreadsheetAccess();
+    const currentDate = new Date().toLocaleDateString();
+
+    rows.forEach(entry => {
+      entry.date = currentDate;
+      entry.applied = 'yes';
+      entry.save();
+    });
+  }
 };
-
-const writeToSpreadsheet = async () => {
-  let rows = await getSpreadsheetAccess();
-  const currentDate = new Date().toLocaleDateString();
-
-  //Need to add in the recruiter name, and recruiter email, and update domain
-  rows.forEach(entry => {
-    entry.date = currentDate;
-    entry.applied = 'yes';
-    entry.save();
-  });
-};
-
-writeToSpreadsheet();
