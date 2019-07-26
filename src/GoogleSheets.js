502d3852a9466d3e711f08dad3652b37d36af867
Your Name <itzalbert2@gmail.com>
2019-07-26 11:45:24 -0700

    Refactor module pattern

--- src/GoogleSheets.js
+++ src/GoogleSheets.js
@@ -3,53 +3,55 @@ const creds = require('../config/client_secret.json');
 const { spreadsheetId } = require('../config/config_test_file');
 const { promisify } = require('util');
 
-module.exports = {
-  /**
-   * @description: This function will automatically grab all of the rows within the first spreadsheet
-   * that you have listed, and returns back an array of objects that contain all the columns/rows.
-   */
+/**
+ * @description: This function will automatically grab all of the rows within the first spreadsheet
+ * that you have listed, and returns back an array of objects that contain all the columns/rows.
+ */
 
-  getSpreadsheetAccess: async () => {
-    const doc = new GoogleSpreadsheet(spreadsheetId);
-    await promisify(doc.useServiceAccountAuth)(creds);
-    const sheetInfo = await promisify(doc.getInfo)();
-    const currentSheet = sheetInfo.worksheets[0];
+const getSpreadsheetAccess = async () => {
+  const doc = new GoogleSpreadsheet(spreadsheetId);
+  await promisify(doc.useServiceAccountAuth)(creds);
+  const sheetInfo = await promisify(doc.getInfo)();
+  const currentSheet = sheetInfo.worksheets[0];
 
-    const rows = await promisify(currentSheet.getRows)({
-      offset: 1,
-      limit: 120,
-      query: 'applied = no and hashyperlink = true'
-    });
-    return rows;
-  },
+  const rows = await promisify(currentSheet.getRows)({
+    offset: 1,
+    limit: 120,
+    query: 'applied = no and hashyperlink = true'
+  });
+  return rows;
+};
 
-  /**
-   *
-   * @description: parseJobLinks is the shell function that returns a list an array of jobs that has
-   * been parsed out of the google sheet. Access to the sheet object is provided via the getSpreadsheetAccess
-   * function
-   */
+/**
+ * @description: parseJobLinks is the shell function that returns a list an array of jobs that has
+ * been parsed out of the google sheet. Access to the sheet object is provided via the getSpreadsheetAccess
+ * function
+ */
 
-  parseJobLinks: async () => {
-    const googleSheetEntries = await getSpreadsheetAccess();
-    const jobLinks = [];
+const parseJobLinks = async () => {
+  const googleSheetEntries = await getSpreadsheetAccess();
+  const jobLinks = [];
 
-    // NOTE: Potential for refactor, as for loops are blocking.
-    for (let i = 0; i < googleSheetEntries.length; i++) {
-      jobLinks.push(googleSheetEntries[i].link);
-    }
-
-    return jobLinks;
-  },
-
-  writeToSpreadsheet: async () => {
-    const rows = await getSpreadsheetAccess();
-    const currentDate = new Date().toLocaleDateString();
-
-    rows.forEach(entry => {
-      entry.date = currentDate;
-      entry.applied = 'yes';
-      entry.save();
-    });
+  // NOTE: Potential for refactor, as for loops are blocking.
+  for (let i = 0; i < googleSheetEntries.length; i++) {
+    jobLinks.push(googleSheetEntries[i].link);
   }
+  console.log(jobLinks);
+  return jobLinks;
+};
+
+const writeToSpreadsheet = async () => {
+  const rows = await getSpreadsheetAccess();
+  const currentDate = new Date().toLocaleDateString();
+
+  rows.forEach(entry => {
+    entry.date = currentDate;
+    (entry.applied = 'yes'), entry.save();
+  });
+};
+
+module.exports = {
+  getSpreadsheetAccess,
+  parseJobLinks,
+  writeToSpreadsheet
 };
