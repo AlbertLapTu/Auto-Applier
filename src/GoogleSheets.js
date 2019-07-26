1e10df1840ef3046edb4c48260d7a58dcc39b22e
Your Name <itzalbert2@gmail.com>
2019-07-26 10:57:29 -0700

    Add parseJobLink function along with cleanup

--- src/GoogleSheets.js
+++ src/GoogleSheets.js
@@ -3,12 +3,12 @@ const creds = require('../config/client_secret.json');
 const { spreadsheetId } = require('../config/config_test_file');
 const { promisify } = require('util');
 
-/**
- * @description: This function will automatically grab all of the rows within the first spreadsheet
- * that you have listed, and returns back an array of objects that contain all the columns/rows.
- */
-
 module.exports = {
+  /**
+   * @description: This function will automatically grab all of the rows within the first spreadsheet
+   * that you have listed, and returns back an array of objects that contain all the columns/rows.
+   */
+
   getSpreadsheetAccess: async () => {
     const doc = new GoogleSpreadsheet(spreadsheetId);
     await promisify(doc.useServiceAccountAuth)(creds);
@@ -20,12 +20,26 @@ module.exports = {
       limit: 120,
       query: 'applied = no and hashyperlink = true'
     });
-
-    /**
-     * @description: Return value rows is an array of spreadsheet items for that specific sheet.
-     */
-
     return rows;
+  },
+
+  /**
+   *
+   * @description: parseJobLinks is the shell function that returns a list an array of jobs that has
+   * been parsed out of the google sheet. Access to the sheet object is provided via the getSpreadsheetAccess
+   * function
+   */
+
+  parseJobLinks: async () => {
+    const googleSheetEntries = await getSpreadsheetAccess();
+    const jobLinks = [];
+
+    // NOTE: Potential for refactor, as for loops are blocking.
+    for (let i = 0; i < googleSheetEntries.length; i++) {
+      jobLinks.push(googleSheetEntries[i].link);
+    }
+
+    return jobLinks;
   },
 
   writeToSpreadsheet: async () => {
