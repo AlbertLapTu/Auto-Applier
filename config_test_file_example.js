e5b94514f2459d4e0d36bb72a41004fb35a967e9
Your Name <itzalbert2@gmail.com>
2019-08-19 20:30:44 -0700

    Recommit test file

---
+++ config_test_file_example.js
@@ -0,0 +1,17 @@
+/**
+ * @param {username} String
+ * @param {password} String
+ * @param {fullName} String
+ * @param {spreadsheetId} String
+ * @param {sheetId} Integer
+ * @param {clearbitKey} String
+ */
+
+module.exports = {
+  username: 'AngelList login',
+  password: 'AngelList password',
+  fullName: 'Your name',
+  spreadsheetId: 'Your Google spreadsheet ID',
+  sheetId: Google_Sheet_Id,
+  clearbitKey: 'Clearbit API Key'
+};
