fd277920f732a65ef14f9d33b0b534d42b918775
Your Name <itzalbert2@gmail.com>
2019-08-19 17:51:17 -0700

    Fix linting issues

--- config_test_file_example.js
+++ config_test_file_example.js
@@ -7,12 +7,11 @@
  * @param {clearbitKey}: Type string
  */
 
-
 module.exports = {
-  username: 'ANGELLIST USERNAME'
-  password: 'ANGELLIST PASSWORD'
-  fullName: 'YOUR NAME'
+  username: 'ANGELLIST USERNAME',
+  password: 'ANGELLIST PASSWORD',
+  fullName: 'YOUR NAME',
   spreadsheetId: 'GOOGLE SPREADSHEET ID',
-  sheetId: GOOGLE ID,
+  sheetId: GOOGLE_ID,
   clearbitKey: 'sk_aacb14c65ea22809049e8c6c64397172'
 };
